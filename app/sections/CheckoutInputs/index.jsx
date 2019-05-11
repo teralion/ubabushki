8960d511b5a122f66b1a1b6b89a350725fff4a41
teralion <bd@whitescape.com>
2019-05-11 20:21:45 +0300

    Add inputs to CheckoutInputs section component, remove Input element component

--- app/sections/CheckoutInputs/index.jsx
+++ app/sections/CheckoutInputs/index.jsx
@@ -1,11 +1,100 @@
-import React from 'react';
-import T from 'prop-types';
+import React, { useState } from 'react';
 
-import cx from 'classnames';
 import css from './index.styl';
 
-export default function CheckoutInputs(props) {
-  return null
+const inputs = [
+  {
+    id: 'name',
+    handler: 'handleName',
+    label: 'Ваше имя',
+    type: 'text',
+  },
+  {
+    id: 'phone',
+    handler: 'handlePhone',
+    label: 'Телефон',
+    type: 'text',
+  },
+  {
+    id: 'address',
+    handler: 'handleAddress',
+    label: 'Адрес доставки',
+    type: 'text',
+  },
+  {
+    id: 'time',
+    handler: 'handleTime',
+    label: 'Время',
+    type: 'time',
+  },
+  {
+    id: 'date',
+    handler: 'handleDate',
+    label: 'Дата',
+    type: 'date',
+  },
+  {
+    id: 'comment',
+    handler: 'handleComment',
+    label: 'Комментарий',
+    type: 'text',
+  },
+];
+
+export default function CheckoutInputs() {
+  const [name, handleName] = useState('');
+  const [phone, handlePhone] = useState('');
+  const [address, handleAddress] = useState('');
+  const [time, handleTime] = useState('');
+  const [date, handleDate] = useState('');
+  const [comment, handleComment] = useState('');
+
+  const state = {
+    name,
+    handleName,
+    phone,
+    handlePhone,
+    address,
+    handleAddress,
+    time,
+    handleTime,
+    date,
+    handleDate,
+    comment,
+    handleComment,
+  };
+
+  return (
+    <>
+      {inputs.map((input) => {
+        const {
+          id,
+          label,
+          handler,
+          type,
+        } = input;
+
+        return (
+          <div
+            key={id}
+            className={css.inputBlock}
+          >
+            <div className={css.label}>
+              { label }
+            </div>
+            <input
+              type={type}
+              value={state[id]}
+              onChange={e => (
+                state[handler]((e.target || {}).value || '')
+              )}
+              className={css.input}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
 }
-CheckoutInputs.propTypes = {}
-CheckoutInputs.defaultProps = {}
+CheckoutInputs.propTypes = {};
+CheckoutInputs.defaultProps = {};
